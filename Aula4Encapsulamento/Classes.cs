2937a21b0768e87f5fd28c98a385d0c3b44fc3b6
Felipe F. Schwade <felipefaeschwade@hotmail.com>
2017-09-21 19:38:12 -0400

    Aplicando o encapsulamento em uma classe.

---
+++ Aula4Encapsulamento/Classes.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula4Encapsulamento
+{
+    public class ProcessadorDeBoletos
+    {
+        public void Processa(IList<Boleto> boletos, Fatura fatura)
+        {
+            foreach (Boleto boleto in boletos)
+            {
+                Pagamento pagamento = new Pagamento(boleto.Valor, MeioDePagamento.BOLETO);
+                fatura.AdicionaPagamento(pagamento);
+            }
+        }
+    }
+
+    public class Boleto
+    {
+        public double Valor { get; private set; }
+
+        public Boleto(double valor)
+        {
+            this.Valor = valor;
+        }
+    }
+
+    public class Fatura
+    {
+        public string Cliente { get; private set; }
+        public double Valor { get; set; }
+        private IList<Pagamento> Pagamentos;
+        private bool Pago;
+
+        public Fatura(string cliente, double valor)
+        {
+            this.Cliente = cliente;
+            this.Valor = valor;
+            this.Pagamentos = new List<Pagamento>();
+            this.Pago = false;
+        }
+
+        public void AdicionaPagamento(Pagamento pagamento)
+        {
+            double total = 0;
+            this.Pagamentos.Add(pagamento);
+
+            if (total >= Valor)
+            {
+                this.Pago = true;
+            }
+        }
+
+    }
+    public enum MeioDePagamento
+    {
+        BOLETO,
+        CARTAO
+    }
+    public class Pagamento
+    {
+        public double Valor { get; private set; }
+        public MeioDePagamento Forma { get; private set; }
+
+        public Pagamento(double valor, MeioDePagamento forma)
+        {
+            // TODO: Complete member initialization
+            this.Valor = valor;
+            this.Forma = forma;
+        }
+    }
+}
